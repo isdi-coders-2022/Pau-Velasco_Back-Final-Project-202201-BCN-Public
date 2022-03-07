c4819597d65302b3d9468d82693f4e0673a28bc2
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-07 22:38:50 +0100

    add generalError function on errors.js

--- src/server/middlewares/errors.js
+++ src/server/middlewares/errors.js
@@ -1,7 +1,18 @@
+const debug = require("debug");
+const chalk = require("chalk");
+
 const notFoundError = (req, res) => {
   res
     .status(404)
     .json({ error: true, message: "Error 404. Endpoint not found" });
+};
+
+// eslint-disable-next-line no-unused-vars
+const generalError = (err, req, res, next) => {
+  debug(chalk.red(`Error: ${err.message}`));
+  const errorCode = err.code ?? 500;
+  const errorMessage = err.code ? err.message : "General peting";
+  res.status(errorCode).json({ error: true, message: errorMessage });
 };
 
 module.exports = notFoundError;
