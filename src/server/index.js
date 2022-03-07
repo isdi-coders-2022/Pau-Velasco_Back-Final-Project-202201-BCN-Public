61bde6392cd87713b3da6beca36e63a2aa962467
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-07 19:08:53 +0100

    add notFoundError middleware on index

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
+const notFoundError = require("./middlewares/errors");
 
 const app = express();
 
@@ -9,5 +10,7 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(helmet());
+
+app.use(notFoundError);
 
 module.exports = app;
