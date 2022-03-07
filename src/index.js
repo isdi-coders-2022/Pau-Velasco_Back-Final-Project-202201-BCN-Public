955c117c3c18d6e638bfbfd3aeea96337cb295ea
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-07 23:41:53 +0100

    initialize databaseConnect on server/index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 require("dotenv").config();
 const debug = require("debug")("futsal-stats: root");
 const chalk = require("chalk");
+const databaseConnect = require("./database");
 const app = require("./server");
 const startServer = require("./server/startServer");
 
 const port = process.env.PORT || 4000;
-
-(async () => {
+const mongoString = process.env.MONGO_STRING(async () => {
   try {
     await startServer(app, port);
+    await databaseConnect(mongoString);
   } catch (error) {
     debug(chalk.bold.red(`Error ${error.message}`));
   }
