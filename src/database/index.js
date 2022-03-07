19aaedda18318f8d7f86d45a599b8354e3ce62f2
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-07 23:39:47 +0100

    add databaseConnnect function

---
+++ src/database/index.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const databaseConnect = (connectionString) =>
+  new Promise((resolve, reject) => {
+    mongoose.set("debug", true);
+    mongoose.set("toJSON", {
+      virtuals: true,
+      transform: (doc, ret) => {
+        // eslint-disable-next-line no-param-reassign, no-underscore-dangle
+        delete ret._id;
+        // eslint-disable-next-line no-param-reassign, no-underscore-dangle
+        delete ret.__v;
+      },
+    });
+  });
