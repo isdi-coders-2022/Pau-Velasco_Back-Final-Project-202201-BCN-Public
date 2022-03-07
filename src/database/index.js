3c79634bf25a5c524afafcf0b7a24e3760047916
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-07 23:39:59 +0100

    add databaseConnect on error

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const debug = require("debug")("twitter: database");
+const chalk = require("chalk");
 
 const databaseConnect = (connectionString) =>
   new Promise((resolve, reject) => {
@@ -11,5 +13,13 @@ const databaseConnect = (connectionString) =>
         // eslint-disable-next-line no-param-reassign, no-underscore-dangle
         delete ret.__v;
       },
+    });
+    mongoose.connect(connectionString, (error) => {
+      if (error) {
+        reject(new Error(`Database not connected: ${error.message}`));
+        return;
+      }
+      debug(chalk.yellow("Database connected"));
+      resolve();
     });
   });
