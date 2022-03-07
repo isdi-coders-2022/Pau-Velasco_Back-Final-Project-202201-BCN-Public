4845ac5a1b7c0f213d4f8c8983eb3a7a96b9cf67
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-07 16:52:47 +0100

    add provisional app on index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const debug = require("debug")("futsal-stats: root");
 const chalk = require("chalk");
+const express = require("express");
 const startServer = require("./server/startServer");
 
 const port = process.env.PORT || 4000;
+const app = express();
 
 (async () => {
   try {
