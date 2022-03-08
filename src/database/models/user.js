ad73d8186abc575a280877c207b794c811290e4c
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-08 09:47:53 +0100

    add userSchema

---
+++ src/database/models/user.js
@@ -0,0 +1,22 @@
+const { Schema } = require("mongoose");
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    maxlength: 12,
+  },
+  teamName: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  players: {
+    type: [Schema.Types.ObjectId],
+    ref: "Player",
+    default: [],
+  },
+});
