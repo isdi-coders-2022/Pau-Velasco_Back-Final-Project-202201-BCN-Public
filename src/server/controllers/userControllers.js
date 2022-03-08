f66eb3a95c5c7b79c65fbc36681364f9db480207
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-08 10:43:57 +0100

    add loginUser controller

---
+++ src/server/controllers/userControllers.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("../../database/models/user");
+
+const loginUser = async (req, res, next) => {
+  const { username, password } = req.body;
+
+  const user = User.findOne({ username });
+
+  if (!user) {
+    const error = new Error("Wrong username, user not found");
+    error.code = 404;
+    return next(error);
+  }
+
+  const rightPassword = await bcrypt.compare(password, user.password);
+  if (!rightPassword) {
+    const error = new Error("Wrong password");
+    error.code = 401;
+    return next(error);
+  }
+
+  const userData = {
+    username: user.username,
+    id: user.id,
+  };
+
+  const token = jwt.sign(userData, process.env.JWT_SECRET);
+  res.json({ token });
+};
