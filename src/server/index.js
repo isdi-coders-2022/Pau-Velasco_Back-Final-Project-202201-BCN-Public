c4d9a6f05ef7e11cc9798d1d6760269b7ad60b19
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-12 19:51:51 +0100

    import playerRouter on index.js

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,8 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { notFoundError, generalError } = require("./middlewares/errors");
-const route = require("./routes/userRoutes");
+const userRouter = require("./routes/userRoutes");
+const playerRouter = require("./routes/playerRoutes");
 
 const app = express();
 
@@ -12,7 +13,8 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(helmet());
 
-app.use("/user", route);
+app.use("/user", userRouter);
+app.use("/player", playerRouter);
 
 app.use(generalError);
 app.use(notFoundError);
