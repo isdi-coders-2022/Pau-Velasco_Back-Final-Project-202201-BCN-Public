5d15cde4d24fd6121d50585f68cc30be0a7059ea
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-13 15:42:24 +0100

    add multer on playerRoutes

--- src/server/routes/playerRoutes.js
+++ src/server/routes/playerRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const multer = require("multer");
 const {
   createPlayer,
   deletePlayer,
@@ -6,8 +7,9 @@ const {
 const auth = require("../middlewares/auth");
 
 const router = express.Router();
+const upload = multer({ dest: "uploads/" });
 
-router.post("/create-player", auth, createPlayer);
+router.post("/create-player", auth, upload.single("photo"), createPlayer);
 router.delete("/delete/:id", auth, deletePlayer);
 
 module.exports = router;
