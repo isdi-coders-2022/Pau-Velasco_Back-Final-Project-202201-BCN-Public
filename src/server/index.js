ce40c0247850c2b5a3db24939d2357dbde5531fd
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-21 22:28:28 +0100

    set helmet on correct position

--- src/server/index.js
+++ src/server/index.js
@@ -8,8 +8,8 @@ const playerRouter = require("./routes/playerRoutes");
 
 const app = express();
 
-app.use(helmet());
 app.use(cors());
+app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.static("public"));
